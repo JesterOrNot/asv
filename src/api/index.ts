1858f11e7ff3a66c775ae67c0c5349aac2ce808d
VottusCode <vottus@vott.us>
2020-10-31 13:31:13 +0100

    Resolves staging api server

--- src/api/index.ts
+++ src/api/index.ts
@@ -9,12 +9,20 @@ export interface BaseResponse {
   data?: object
 }
 
+export const isStaging = () => window.location.hostname.startsWith("staging")
+
+export const resolveApiDomain = () => {
+  if (process.env.NODE === "development") return "http://localhost:4000/api/v1"
+  return isStaging()
+    ? "https://staging-api.asvgroup.cz/v1"
+    : "https://api.asvgroup.cz/v1"
+}
+
 export const axi = () => {
+  console.log("[ASV Axi] Staging Server:", isStaging())
+  console.log("[ASV Axi] Chosen API:", resolveApiDomain())
   const axios = Axios
-  axios.defaults.baseURL =
-    process.env.NODE_ENV === "development"
-      ? "http://localhost:4000/api/v1"
-      : "https://api.asvgroup.cz/v1"
+  axios.defaults.baseURL = resolveApiDomain()
   const token = localStorage.getItem("asv_token")
 
   if (typeof token == "string")
