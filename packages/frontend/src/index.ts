0a352be744c3b42045caa4a3f7e7e60c953fef03
Mia <vottus@vott.us>
2021-04-02 16:06:10 +0200

    feat: redesign

--- packages/frontend/src/index.ts
+++ packages/frontend/src/index.ts
@@ -2,7 +2,8 @@ import { createApp } from "vue"
 import App from "./App.vue"
 import router from "./router"
 
-import "./assets/scss/main.scss"
+// import "./assets/scss/main.scss"
+import "./assets/tailwind.css"
 
 const mountEl = document.querySelector("#root")
 if (!mountEl) throw new Error("No mount element found.")
